44c06476626f60f54d3b7210616a3e6c62e6d812
Sean Ryan <seanpryan89@gmail.com>
2018-10-19 01:07:45 -0400

    added cleanStrings function

--- scripts.js
+++ scripts.js
@@ -7,30 +7,30 @@ var markup1;
 var myUrl = 'https://dl.acm.org/citation.cfm?id=3240360';
 
 var urls = [
-    'http://dl.acm.org/citation.cfm?id=3240357',
-    'http://dl.acm.org/citation.cfm?id=3240375',
-    'http://dl.acm.org/citation.cfm?id=3240381',
-    'http://dl.acm.org/citation.cfm?id=3241852',
-    'http://dl.acm.org/citation.cfm?id=3241865',
+'http://dl.acm.org/citation.cfm?id=3240357',
+'http://dl.acm.org/citation.cfm?id=3240375',
+'http://dl.acm.org/citation.cfm?id=3240381',
+'http://dl.acm.org/citation.cfm?id=3241852',
+'http://dl.acm.org/citation.cfm?id=3241865',
 ]
 
 function getData(myUrl, callback) {
-    return $.getJSON(
-        'https://allorigins.me/get?url=' + encodeURIComponent(myUrl) + '&callback=?',
-        callback,
-    );
+	return $.getJSON(
+		'https://allorigins.me/get?url=' + encodeURIComponent(myUrl) + '&callback=?',
+		callback,
+		);
 }
 
 const test = (url) => {
-    return new Promise((resolve, reject) => {
-        getData(url, data => resolve(data));
-    });
+	return new Promise((resolve, reject) => {
+		getData(url, data => resolve(data));
+	});
 }
 
 function regexFunction(resolvedData) {
-    var arr = resolvedData.contents.match(/(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm);
+	var arr = resolvedData.contents.match(/(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm);
 
-    return arr;
+	return arr;
 }
 
 
@@ -39,6 +39,44 @@ let results = Promise.all(promises).then(data => data.map(regexFunction));
 console.log(results);
 
 
+// get clean data out of Regex strings
+var arr = [
+
+"<title>Causal embeddings for recommendation</title>",
+"\"Author Profile Page\" target=\"_self\">Stephen Bonner</a>",
+"\"Author Profile Page\" target=\"_self\">Flavian Vasile</a>",
+"\"Conference Website\" target=\"_blank\"><img style=\"margin-top:5px; border-width:1px; border-color:black\" src=\"https://portalparts.acm.org/3250000/3240323/thumb/cover_thumb.jpg\" height=\"102\" width=\"79\" alt=\"Cover Image\" /></a>",
+"\"Conference Website\" target=\"_self\" class=\"link-text\">RecSys '18</a>",
+"award.jpg\" alt=\"Best Paper\" "
+
+]
+
+function cleanStrings(arr){
+
+	var title = arr[0].replace("<title>", "").replace("</title>", "");
+	var award = arr[arr.length -1].replace("award.jpg\" alt=\"", "").replace("\" ", "");
+	var conference = arr[arr.length - 2].replace("\"Conference Website\" target=\"_self\" class=\"link-text\">", "").replace("</a>", "");
+	var authorsNumber = arr.length - 4;
+	var authors = [];
+
+	for (i = 1; i < authorsNumber + 1; i++){
+		var author = arr[i].replace("\"Author Profile Page\" target=\"_self\">", "").replace("</a>", "");
+
+		if (i != 1){
+			authors.push(" " + author);
+		} else {
+
+			authors.push(author);
+		}
+	}
+
+	console.log("this is the title " + title);
+	console.log("this is the award " + award);
+	console.log("this is the conference " + conference);
+	console.log("these are the authors " + authors);
+}
+
+cleanStrings(arr);
 
 // //loads JSON-encoded data from the server using a GET HTTP request. Saves as variable.
 // var markup1;
