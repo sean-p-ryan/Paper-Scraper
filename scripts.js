d00958d9e35d5203a4e393a135d051cfd2657d2c
Sean Ryan <seanpryan89@gmail.com>
2018-10-18 20:55:54 -0400

    still working on  script

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 //saves full source code of page to variable
-var url1 = $(".url1").val()
+var url1 = $(".url1").val();
 
 //loads JSON-encoded data from the server using a GET HTTP request. Saves as variable.
 var markup1 = $.getJSON('https://allorigins.me/get?url=' + encodeURIComponent('https://dl.acm.org/citation.cfm?id=3240360') + '&callback=?', function(data){
-    return data.contents;
+    return data;
 });
+
+var arr = markup1.responseJSON.contents.match(/(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm);
 
 //capture paper title, award name, author names and conference names from web page markup inside JSON-encoded data
 
