ef8b6a57002eb65746729e738cbbc89c3c0afa01
William Dana <william.b.dana@gmail.com>
2018-10-21 18:50:25 -0400

    Add table rows

--- scripts.js
+++ scripts.js
@@ -3,15 +3,14 @@
 
 
 //loads JSON-encoded data from the server using a GET HTTP request. Saves as variable.
-var markup1;
 var myUrl = 'https://dl.acm.org/citation.cfm?id=3240360';
 
 var urls = [
-'http://dl.acm.org/citation.cfm?id=3240357',
-'http://dl.acm.org/citation.cfm?id=3240375',
-'http://dl.acm.org/citation.cfm?id=3240381',
-'http://dl.acm.org/citation.cfm?id=3241852',
-'http://dl.acm.org/citation.cfm?id=3241865',
+	'http://dl.acm.org/citation.cfm?id=3240357',
+	'http://dl.acm.org/citation.cfm?id=3240375',
+	'http://dl.acm.org/citation.cfm?id=3240381',
+	'http://dl.acm.org/citation.cfm?id=3241852',
+	'http://dl.acm.org/citation.cfm?id=3241865',
 ]
 
 //pull data from paper web page on Digital Library
@@ -19,7 +18,7 @@ function getData(myUrl, callback) {
 	return $.getJSON(
 		'https://allorigins.me/get?url=' + encodeURIComponent(myUrl) + '&callback=?',
 		callback,
-		);
+	);
 }
 
 const test = (url) => {
@@ -35,22 +34,45 @@ function regexFunction(resolvedData) {
 	return arr;
 }
 
-
+let cleaned = [];
 let promises = urls.map(test);
-let results = Promise.all(promises).then(data => data.map(regexFunction));
-console.log(results);
+let results =
+	Promise.all(promises)
+		.then(data => {
+			return data.map(regexFunction)
+		})
+		.then(regexes => {
+			console.log(cleanStrings);
+			return regexes.map(regex => {
+				return cleanStrings(regex);
+			});
+		})
+		// .then(results => {
+		// 	results.forEach(result => {
+		// 		cleaned.push(result);
+		// 	});
+		// });
+		.then(results => {
+			return mapUrls(urls, results);
+		})
+		.then(tableRows => {
+			const table = $("#table-body");
+			return tableRows.forEach(row => {
+				table.append(`<tr><td>${row.url}</td><td>${row.title}</td><td>${row.award}</td><td>${row.authors}</td><td>${row.conference}</td></tr>`);
+			})
+		})
+
+// console.log(results);
 
 
 // get clean data out of Regex strings
 var arr = [
-
-"<title>Causal embeddings for recommendation</title>",
-"\"Author Profile Page\" target=\"_self\">Stephen Bonner</a>",
-"\"Author Profile Page\" target=\"_self\">Flavian Vasile</a>",
-"\"Conference Website\" target=\"_blank\"><img style=\"margin-top:5px; border-width:1px; border-color:black\" src=\"https://portalparts.acm.org/3250000/3240323/thumb/cover_thumb.jpg\" height=\"102\" width=\"79\" alt=\"Cover Image\" /></a>",
-"\"Conference Website\" target=\"_self\" class=\"link-text\">RecSys '18</a>",
-"award.jpg\" alt=\"Best Paper\" "
-
+	"<title>Causal embeddings for recommendation</title>",
+	"\"Author Profile Page\" target=\"_self\">Stephen Bonner</a>",
+	"\"Author Profile Page\" target=\"_self\">Flavian Vasile</a>",
+	"\"Conference Website\" target=\"_blank\"><img style=\"margin-top:5px; border-width:1px; border-color:black\" src=\"https://portalparts.acm.org/3250000/3240323/thumb/cover_thumb.jpg\" height=\"102\" width=\"79\" alt=\"Cover Image\" /></a>",
+	"\"Conference Website\" target=\"_self\" class=\"link-text\">RecSys '18</a>",
+	"award.jpg\" alt=\"Best Paper\" ",
 ]
 
 function cleanStrings(arr){
@@ -75,6 +97,19 @@ function cleanStrings(arr){
 	console.log("THE AWARD NAME IS: " + award);
 	console.log("THE CONFERENCE NAME IS: " + conference);
 	console.log("THE AUTHORS ARE: " + authors);
+	return {
+		award,
+		conference,
+		authorsNumber,
+		authors,
+		title,
+	}
+}
+
+function mapUrls(urls, resultsObjectsArray) {
+	return resultsObjectsArray.map((resultsObject, index) => {
+		return Object.assign({}, resultsObject, { url: urls[index] });
+	});
 }
 
 cleanStrings(arr);
