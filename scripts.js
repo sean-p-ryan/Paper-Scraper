6970aab694d8c3d32ad809aebc946f3fef78ea0e
William Dana <william.b.dana@gmail.com>
2018-10-22 20:06:26 -0400

    Work on new approach for regex

--- scripts.js
+++ scripts.js
@@ -29,9 +29,69 @@ const test = (url) => {
 
 //pulls only needed strings out of webpage markup with Regex
 function regexFunction(resolvedData) {
-	var arr = resolvedData.contents.match(/(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm);
+	// console.log("RESOLVED DATA");
+	// console.log(resolvedData.contents);
+	// return resolvedData.contents.match(/(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm);
+	let html = resolvedData.contents;
 
-	return arr;
+	let rx = /(<title>(.*)<\/title>)|"Conference Website"(.*)>(.*)<\/a>|"Author Profile Page"(.*)>(.*)<\/a>|award.*alt="(.*)"\s/gm;
+
+	let award = '';
+	let authors = [];
+	let conference = '';
+	let title = '';
+
+	while (rx.exec(html) !== null) {
+		let iterationPass = rx.exec(html);
+		console.log(iterationPass);
+
+		if (iterationPass === null) {
+			break;
+		}
+
+		iterationPass = iterationPass.filter(result => {
+			return result !== undefined;
+		});
+
+		console.log(iterationPass);
+
+		if (iterationPass.some(result => {
+			return result.includes('<title>');
+		})) {
+			console.log('title hit');
+			title = iterationPass[iterationPass.length - 1];
+		}
+
+		else if (iterationPass.some(result => {
+			return result.includes('Conference Website')
+		})) {
+			console.log('conf hit');
+			conference = iterationPass[iterationPass.length - 1];
+		}
+
+		else if (iterationPass.some(result => {
+			return result.includes('award')
+		})) {
+			console.log('award hit');
+			award = iterationPass[iterationPass.length - 1];
+		}
+
+		else if (iterationPass.some(result => {
+			return result.includes('Author')
+		})) {
+			console.log('author hit');
+			authors.push(iterationPass[iterationPass.length - 1]);
+		}
+	}
+
+	let result = [
+		award,
+		authors,
+		conference,
+		title,
+	];
+	console.log(result);
+	return result;
 }
 
 let cleaned = [];
@@ -42,7 +102,6 @@ let results =
 			return data.map(regexFunction)
 		})
 		.then(regexes => {
-			console.log(cleanStrings);
 			return regexes.map(regex => {
 				return cleanStrings(regex);
 			});
@@ -58,6 +117,7 @@ let results =
 		.then(tableRows => {
 			const table = $("#table-body");
 			return tableRows.forEach(row => {
+				console.log(row);
 				table.append(`<tr><td>${row.url}</td><td>${row.title}</td><td>${row.award}</td><td>${row.authors}</td><td>${row.conference}</td></tr>`);
 			})
 		})
@@ -85,7 +145,7 @@ function cleanStrings(arr){
 	for (i = 1; i < authorsNumber + 1; i++){
 		var author = arr[i].replace("\"Author Profile Page\" target=\"_self\">", "").replace("</a>", "");
 
-		if (i != 1){
+		if (i !== 1){
 			authors.push(" " + author);
 		} else {
 
@@ -93,10 +153,6 @@ function cleanStrings(arr){
 		}
 	}
 
-	console.log("THE TITLE IS: " + title);
-	console.log("THE AWARD NAME IS: " + award);
-	console.log("THE CONFERENCE NAME IS: " + conference);
-	console.log("THE AUTHORS ARE: " + authors);
 	return {
 		award,
 		conference,
@@ -112,6 +168,5 @@ function mapUrls(urls, resultsObjectsArray) {
 	});
 }
 
-cleanStrings(arr);
 
 
